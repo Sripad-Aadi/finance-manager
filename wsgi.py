40a4f4e759fdc69712ec855237d1d4f24f232075
AADI SRIPAD <sripadadhi135@gmail.com>
2026-01-29 23:17:51 +0530

    Initialize SQLite DB on Render startup

--- wsgi.py
+++ wsgi.py
@@ -14,10 +14,13 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-from app import create_app
+from app import create_app,db
 
 # Create application instance
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
