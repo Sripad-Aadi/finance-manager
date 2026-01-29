639bec3bcef308cabdecb2ef27994db4d1d3a977
AADI SRIPAD <sripadadhi135@gmail.com>
2026-01-30 00:03:08 +0530

    fixed the security headers

--- app/__init__.py
+++ app/__init__.py
@@ -77,10 +77,15 @@ def create_app(config_class=None):
     # Security headers and cache control
     @app.after_request
     def set_security_headers(response):
+        if response is None:
+            return response
+
         response.headers['X-Content-Type-Options'] = 'nosniff'
         response.headers['X-Frame-Options'] = 'SAMEORIGIN'
         response.headers['X-XSS-Protection'] = '1; mode=block'
         response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
+
+        return response
         
     
     # Setup logging
